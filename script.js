9f7c16e8f99162c04ae1b6b32181d7b44f685c6c
software-punduit <72712348+software-punduit@users.noreply.github.com>
2022-06-24 17:58:01 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let first = parseInt(prompt("Enter your first number"));
+let operator = prompt("Enter operator you wish to use from the list below: +, -, *, /, %");
+let second = parseInt(prompt("Enter your second number"));
+
+
+if (operator === "+") {
+    let addition = first + second;
+    alert(addition);
+} else if(operator ==="-"){
+    let subtraction = first - second;
+    alert(subtraction);
+} else if(operator ==="*"){
+    let multiplication = first * second;
+    alert(multiplication);
+} else if(operator ==="/"){
+    let division = first / second;
+    alert(division);
+} else if(operator ==="%"){
+    let modulus = first % second;
+    alert(modulus);
+} else (
+    alert("'Wrong Input! Refresh the page and provide correct details'"))
